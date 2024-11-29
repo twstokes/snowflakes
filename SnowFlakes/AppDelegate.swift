ba0896b1deaca02ae2aaebeb823a396060855015
Tanner W. Stokes <tanner@tannr.com>
2024-11-29 10:24:43 -0500

    Remove local storage.

--- SnowFlakes/AppDelegate.swift
+++ SnowFlakes/AppDelegate.swift
@@ -2,15 +2,13 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private lazy var stateStore = UserDefaultsStore()
     private lazy var statusItem = StatusItem()
     private lazy var renderer = SnowRenderer()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let appSettings = (try? stateStore.loadAppState()) ?? AppSettings.defaults
+        let appSettings = AppSettings.defaults
         let settingsView = SettingsView(appSettings: appSettings).toVC()
         statusItem.setup(contentVC: settingsView)
-        stateStore.observe(appSettings)
         renderer.observe(appSettings)
     }
 
